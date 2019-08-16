e20be8b08d6a82e11388f38e152fda53d4887319
jmdatulsaxena <jmdatul@gmail.com>
2019-08-16 02:12:56 -0400

    Updates

---
+++ precache-manifest.5aa7319f28a98c45e015fd1a3dfd65d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893834a9e25027462b9885647c240910",
+    "url": "/werestaurant/index.html"
+  },
+  {
+    "revision": "d3e2b981f0c7f4203997",
+    "url": "/werestaurant/static/css/main.3c088988.chunk.css"
+  },
+  {
+    "revision": "b43529b8e5bf05d99d2c",
+    "url": "/werestaurant/static/js/2.b335b0f1.chunk.js"
+  },
+  {
+    "revision": "d3e2b981f0c7f4203997",
+    "url": "/werestaurant/static/js/main.3ab684c6.chunk.js"
+  },
+  {
+    "revision": "094ba87ac0d6b8a73365",
+    "url": "/werestaurant/static/js/runtime~main.75480e22.js"
+  }
+]);
